05b332e6f04aab99b840a83487cec249e11ab4c5
Egor Arndt <egor.arndt98@gmail.com>
2022-03-01 15:52:52 +0400

    started fixing imports caused build to fail, the error is in different spelling in imports in ide and on github in files

--- src/components/layouts/base/index.ts
+++ src/components/layouts/base/index.ts
@@ -1,6 +1,6 @@
 export { default as HeaderBase } from './header'
 export { default as FooterBase } from './footer'
 export { default as Main } from './main'
-export { default as AppContainer } from './appContainer'
+export { default as AppContainer } from './app_container'
 
 export type { HeaderBaseProps } from './header'
