ab7364531a5d4002b524f7484abe6ff4c3340765
Egor Arndt <egor.arndt98@gmail.com>
2022-03-01 15:37:36 +0400

    fixed an err

--- src/components/layouts/base/index.ts
+++ src/components/layouts/base/index.ts
@@ -1,6 +1,6 @@
 export { default as HeaderBase } from './header'
-export { default as FooterBase } from './Footer'
-export { default as Main } from './Main'
-export { default as AppContainer } from './AppContainer'
+export { default as FooterBase } from './footer'
+export { default as Main } from './main'
+export { default as AppContainer } from './appContainer'
 
 export type { HeaderBaseProps } from './header'
